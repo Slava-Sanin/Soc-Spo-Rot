e5c18a3d15fe3535c4523b585eb37963a4a78a9b
Slava-Sanin <slava.sanin@gmail.com>
2017-09-03 10:56:33 +0300

    git status ok git -commit -A -m "deleting not needed files"

--- test/test1.php
+++
@@ -1,8 +0,0 @@
-<?php
-$str = "  <23h1>5Hel@lo World! <scr%^-_+=\\|&:'*()ipt ></sc.ript> 123  sdf 456</23h1>fgg55  ";
-$newstr = filter_var($str, FILTER_SANITIZE_STRING);
-echo $newstr;
-echo "<br><br>";
-$newstr = str_replace(' ','',$newstr);
-echo $newstr;
-?>
